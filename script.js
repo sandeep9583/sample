2bc3d2440bdd2cef03d074064ad26a5c4a6685c9
Sandeep Bandaru <46223856+sandeep9583@users.noreply.github.com>
2025-02-08 13:04:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ let isEdge = /Edg\//.test(navigator.userAgent);
 
         if (isEdge) {
             speechSynthesis.onvoiceschanged = loadEdgeVoice;
-            initializeContent('content-wrap');
+            initializeContent('page-content');
         }
 
 
